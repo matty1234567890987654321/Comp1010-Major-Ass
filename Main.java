5a8afa6648656f21008f5b26d2e7d70b4bc43754
alankimmmm129 <alan.kim@students.mq.edu.au>
2024-10-26 16:43:53 +1100

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,24 +2,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        //Artist instances
+        // Create Artist instances
         Artist johnLennon = new Artist("John Lennon");
         Artist queen = new Artist("Queen");
         Artist eagles = new Artist("Eagles");
-        Artist Kanye = new Artist("Kanye");
+        Artist kanye = new Artist("Kanye");
 
-        //Song list/instances
+        // Create a list of songs and add Song instances to the list
         ArrayList<Songs> songList = new ArrayList<>();
         songList.add(new Songs("Imagine", "3:04", johnLennon, "Imagine"));
-        songList.add(new Songs("Niggas in Parris", "3:04", Kanye, "Fein"));
-        songList.add(new Songs("Niggas", "3:04", Kanye, "Feiner"));
+        songList.add(new Songs("Niggas in Paris", "3:04", kanye, "Fein"));
+        songList.add(new Songs("Niggas", "3:04", kanye, "Feiner"));
         songList.add(new Songs("Bohemian Rhapsody", "5:55", queen, "A Night at the Opera"));
         songList.add(new Songs("Hotel California", "6:30", eagles, "Hotel California"));
 
         // Initialize the library with the song list
         Library library = new Library(songList);
         
-        // Start the initial prompt to ask if the user wants to search by artist
+        // Start the library prompt for user interaction
         library.start();
     }
 }
+
