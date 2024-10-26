f706c9d203dab6bb08ae986b5411d2960bc5a3f7
alankimmmm129 <alan.kim@students.mq.edu.au>
2024-10-26 16:43:07 +1100

    Add files via upload

--- Album.java
+++ Album.java
@@ -1,34 +1,37 @@
 public class Album {
-    private String albumTitle;
-    private Artist artist;
-    private Songs[] songList;  // Array to store the songs in the album
-    private int songCount = 0;
+    private String albumTitle; // Title of the album
+    private Artist artist; // Artist of the album
+    private Songs[] songsArray;  // Array to store the songs in the album
+    private int songCount = 0; // Tracks the current number of songs in the album
 
-    // Constructor to initialize the album with title and artist
+    // Constructor to initialize the album with title, artist, and maximum song capacity.
     public Album(String albumTitle, Artist artist, int maxSongs) {
         this.albumTitle = albumTitle;
         this.artist = artist;
-        this.songList = new Songs[maxSongs];  // Initialize song array
+        this.songsArray = new Songs[maxSongs]; // Initialize song array with given capacity
     }
 
-    // Method to add a song to the album's song array
+    // Adds a song to the album if there is space available.
     public void addSong(Songs song) {
-        if (songCount < songList.length) {
-            songList[songCount++] = song;
+        if (songCount < songsArray.length) {
+            songsArray[songCount++] = song; // Add song and increase count
         } else {
-            System.out.println("Song array is full!");
+            System.out.println("Song array is full!"); // Notify if array is at capacity
         }
     }
 
+    // Retrieves the album title.
     public String getAlbumTitle() {
         return albumTitle;
     }
 
+    // Retrieves the artist of the album.
     public Artist getArtist() {
         return artist;
     }
 
-    public Songs[] getSongList() {
-        return songList;
+    // Retrieves the list of songs in the album.
+    public Songs[] getSongsArray() {
+        return songsArray;
     }
 }
