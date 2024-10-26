cd96bc158aa5b670a0c4b489a34e0cae73511db1
sonya <161278938+sonyakimmq@users.noreply.github.com>
2024-10-26 22:28:46 +1100

    Artist.java

--- Artist.java
+++ Artist.java
@@ -1,14 +1,32 @@
 public class Artist {
     private String name; // Name of the artist
+    private Song[] songs; // Array to store songs by the artist
+    private int songCount; // Tracks the current number of songs
 
-    // Constructor to initialize the artist's name
-    public Artist(String name) {
+    // Constructor to initialize the artist's name and song capacity
+    public Artist(String name, int maxSongs) {
         this.name = name;
+        this.songs = new Song[maxSongs]; // Initialize the song array
+        this.songCount = 0; // Start with no songs
     }
 
     // Retrieves the artist's name
     public String getName() {
         return name;
+    }
+
+    // Adds a song to the artist's song list if there's space available
+    public void addSong(Song song) {
+        if (songCount < songs.length) {
+            songs[songCount++] = song; // Add song and increase count
+        } else {
+            System.out.println("Song array is full for artist: " + name);
+        }
+    }
+
+    // Retrieves the list of songs by the artist
+    public Song[] getSongs() {
+        return songs;
     }
 
     // Returns the artist's name as a string
