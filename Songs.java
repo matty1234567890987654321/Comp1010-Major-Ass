39f4dab5ce7ec59b70c729339e1864673902fbe7
alankimmmm129 <alan.kim@students.mq.edu.au>
2024-10-26 16:44:36 +1100

    Add files via upload

--- Songs.java
+++ Songs.java
@@ -4,6 +4,7 @@ public class Songs {
     private Artist artist;
     private String album;
 
+    // Constructor to initialize a song with title, duration, artist, and album
     public Songs(String title, String duration, Artist artist, String album) {
         this.title = title;
         this.duration = duration;
@@ -11,22 +12,27 @@ public class Songs {
         this.album = album;
     }
 
+    // Getter for song title
     public String getTitle() {
         return title;
     }
 
+    // Getter for song duration
     public String getDuration() {
         return duration;
     }
 
+    // Getter for the artist who performed the song
     public Artist getArtist() {
         return artist;
     }
 
+    // Getter for album, returns "Single" if no album name is provided
     public String getAlbum() {
         return album != null ? album : "Single";
     }
 
+    // Overrides toString to return a  description of the song
     public String toString() {
         return "Song: " + title + " | Duration: " + duration + " | Artist: " + artist.toString() + " | Album: " + getAlbum();
     }
