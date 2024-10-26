a6be1d23cb69b7149b0bfef1fcae0feb5c02400b
alankimmmm129 <alan.kim@students.mq.edu.au>
2024-10-26 16:43:24 +1100

    Add files via upload

--- Artist.java
+++ Artist.java
@@ -1,14 +1,17 @@
 public class Artist {
-    private String name;
+    private String name; // Name of the artist
 
+    // Constructor to initialize the artist's name
     public Artist(String name) {
         this.name = name;
     }
 
+    // Retrieves the artist's name
     public String getName() {
         return name;
     }
 
+    // Returns the artist's name as a string
     public String toString() {
         return name;
     }
