2f26074458eb13c5779f24bd189b49409b4b8cd5
sonya <161278938+sonyakimmq@users.noreply.github.com>
2024-10-26 23:35:12 +1100

    Main.java

--- Main.java
+++ Main.java
@@ -9,18 +9,17 @@ public class Main {
         Artist kanye = new Artist("Kanye");
 
         // Create a list of songs and add Song instances to the list
-        ArrayList<Songs> songList = new ArrayList<>();
-        songList.add(new Songs("Imagine", "3:04", johnLennon, "Imagine"));
-        songList.add(new Songs("Niggas in Paris", "3:04", kanye, "Fein"));
-        songList.add(new Songs("Niggas", "3:04", kanye, "Feiner"));
-        songList.add(new Songs("Bohemian Rhapsody", "5:55", queen, "A Night at the Opera"));
-        songList.add(new Songs("Hotel California", "6:30", eagles, "Hotel California"));
+        ArrayList<Song> songList = new ArrayList<>();
+        songList.add(new Song("Imagine", "3:04", johnLennon, "Imagine"));
+        songList.add(new Song("Niggas in Paris", "3:04", kanye, "Fein"));
+        songList.add(new Song("Niggas", "3:04", kanye, "Feiner"));
+        songList.add(new Song("Bohemian Rhapsody", "5:55", queen, "A Night at the Opera"));
+        songList.add(new Song("Hotel California", "6:30", eagles, "Hotel California"));
 
         // Initialize the library with the song list
         Library library = new Library(songList);
-        
+
         // Start the library prompt for user interaction
         library.start();
     }
 }
-
